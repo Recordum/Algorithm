efab1efc33ec1206ead01fc71bc3472a825bb904
Mingyu <92289805+Recordum@users.noreply.github.com>
2022-11-30 11:42:19 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+class Solution {
+    static int[] number = { -2,3,0,2,-5};
+    static int answer = 0;
+
+    static ArrayList<Integer> list = new ArrayList<>();
+
+    public static void recursion(int start){
+        if(list.size() == 3){
+            int result = list.get(0) +list.get(1)+list.get(2);
+            if(result == 0){
+                answer++;
+                return;
+            }
+        }
+        for( int i = start ; i < number.length; i++){
+
+            list.add(number[i]);
+            start++;
+            recursion(start);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        recursion(0);
+        System.out.println(answer);
+
+    }
+
+}
