fe758df677132c1a01ab35a635892563aa4e490a
Mingyu <92289805+Recordum@users.noreply.github.com>
2022-12-08 22:49:20 +0900

    BOJ1463 1로만들기

--- 05 BinarySearch/Main.java
+++ 05 BinarySearch/Main.java
@@ -1,42 +1,25 @@
-import com.sun.jdi.request.InvalidRequestStateException;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-    static ArrayList<ArrayList<Integer>> dp= new ArrayList<>();
-    public static void fibo(int n){
+    static int[] dp = new int[1000001];
 
-            dp.get(0).set(0 ,1);
-            dp.get(1).set(1 ,1);
-
-        for(int i = 2; i <= n; i++) {
-            dp.get(i).set(0,dp.get(i-1).get(0)+dp.get(i-2).get(0));
-            dp.get(i).set(1,dp.get(i-1).get(1)+dp.get(i-2).get(1));
-        }
-
-    }
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
-        int tc = scanner.nextInt();
-        for(int i = 0 ; i < 41 ; i++){
-         dp.add(new ArrayList<>());
-         dp.get(i).add(0,0);
-         dp.get(i).add(1,0);
+        for(int i = 2; i <= n ; i++){
+        dp[i] = dp[i-1] + 1;
+
+        if(i % 2 == 0){
+            dp[i] = Math.min(dp[i],dp[i/2]+1);
         }
-        int[] n = new int[tc];
-        for(int i = 0 ; i < tc; i++){
-            n[i]  = scanner.nextInt();
-            fibo(n[i]);
+        if(i % 3 == 0){
+            dp[i] = Math.min(dp[i],dp[i/3]+1);
         }
-        for(int i = 0 ; i < tc; i++){
-                System.out.print(dp.get(n[i]).get(0) + " " + dp.get(n[i]).get(1));
-                System.out.println("");
+
         }
+        System.out.println(dp[n]);
 
     }
 }
