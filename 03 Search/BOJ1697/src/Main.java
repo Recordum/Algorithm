233403c49c43802e9777bc15a3d515ce8d61f726
Mingyu <92289805+Recordum@users.noreply.github.com>
2022-12-18 01:35:13 +0900

    BOJ 1697 숨바꼭질

--- 03 Search/BOJ1697/src/Main.java
+++ 03 Search/BOJ1697/src/Main.java
@@ -1,24 +1,61 @@
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+class Node{
+    private int x;
+    private int depth;
+    public Node(int x, int depth ) {
+        this.x = x;
+        this.depth = depth;
+    }
 
+    public int getX() {
+        return x;
+    }
+    public int getDepth(){
+        return depth;
+    }
+
+}
 public class Main {
+    static boolean[] check = new boolean[100001];
+    public static int bfs(int n , int m){
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(n, 0));
+        check[n] =true;
+        while (!queue.isEmpty()){
+            Node node = queue.poll();
+            int x = node.getX();
+            int depth = node.getDepth();
+            if(x == m){
+                return depth;
+            }
+            if(2 * x <= 100000 && check[2*x] == false) {
+                check[2*x] = true;
+                queue.offer(new Node(2 * x, depth + 1));
+            }
+            if (x + 1 <= 100000 && check[x+1] == false) {
+                check[x+1] = true;
+                queue.offer(new Node(x + 1, depth + 1));
+            }
+            if( x-1 >= 0 && check[x-1] ==false) {
+                check[x-1] = true;
+                queue.offer(new Node(x - 1, depth + 1));
+            }
+            if(2*x == m || x+1 == m || x-1 ==m){
+                return depth + 1;
+            }
+        }
+        return -1;
+        }
     public static void main(String[] args) {
-        Queue<Node> queue
+
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
 
-        dp[n] = 0;
-        for(int i = n +1 ; i < m +1 ; i++){
-            dp[i] = dp[i-1] + 1;
-
-            if(i / dp[i-1] == 2){
-                dp[i] = Math.min(dp[i/2] + 1, dp[i]);
-            }
-            dp[i] = Math.min(dp[i+1] + 1 ,dp[i]);
-
-        }
-        System.out.println(dp[m]);
+        int result = bfs(n,m);
+        System.out.println(result);
 
     }
 }
