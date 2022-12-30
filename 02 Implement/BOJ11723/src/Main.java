9236f6dda016fdf4cd469f272d06902b6732fb7b
Mingyu <92289805+Recordum@users.noreply.github.com>
2022-12-31 01:34:32 +0900

    BOJ11723 집합

--- 02 Implement/BOJ11723/src/Main.java
+++ 02 Implement/BOJ11723/src/Main.java
@@ -7,48 +7,53 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<Integer> setList = new ArrayList<>();
-    static StringBuilder outputList = new StringBuilder();
-
+    static ArrayList<LinkedList<Integer>> setList = new ArrayList<>();
+    static ArrayList<Integer> outputList = new ArrayList<>();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
     public static void set(String function, int element){
         switch (function) {
 
             case "add" :
-                setList.set(element, 1);
+                if (setList.get(element).size() == 0) {
+                    setList.get(element).add(element);
+                }
                 break;
 
             case "remove" :
-                setList.set(element,0);
+                if (setList.get(element).size() != 0) {
+                    setList.get(element).remove(0);
+                }
                 break;
 
             case "check" :
-                if (setList.get(element) == 0) {
-                    outputList.append("0\n");
+                if (setList.get(element).size() == 0) {
+                    sb.append("0\n");
                 } else {
-                    outputList.append("1\n");
+                    sb.append("1\n");
                 }
                 break;
 
             case "toggle" :
-                if (setList.get(element) == 0) {
-                    setList.set(element,1);
+                if (setList.get(element).size() == 0) {
+                    setList.get(element).add(element);
                 } else {
-                    setList.set(element,0);
+                    setList.get(element).remove(0);
                 }
                 break;
 
             case "all" :
                 for (int i = 1; i < 21; i++) {
-                    if (setList.get(i) == 0) {
-                        setList.set(i, 1);
+                    if (setList.get(i).size() == 0) {
+                        setList.get(i).add(0, i);
                     }
                 }
                 break;
 
             case "empty" :
                 for (int i = 1; i < 21; i++) {
-                    if (setList.get(i) != 0) {
-                        setList.set(i, 0);
+                    if (setList.get(i).size() != 0) {
+                        setList.get(i).remove(0);
                     }
                 }
 
@@ -59,10 +64,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
+
         for(int i = 0 ; i < 21 ; i++){
-            setList.add(0);
+            setList.add(new LinkedList<Integer>());
         }
         for(int i = 0 ; i < n ; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -75,6 +81,6 @@ public class Main {
             set(function,element);
 
         }
-        System.out.println(outputList);
+        System.out.println(sb);
     }
 }
