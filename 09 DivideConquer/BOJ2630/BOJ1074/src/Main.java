66e2aa3be0ea903c816d04c2888573c806034c5c
Mingyu <92289805+Recordum@users.noreply.github.com>
2023-01-02 01:15:42 +0900

    BOJ1074 Z

--- 09 DivideConquer/BOJ2630/BOJ1074/src/Main.java
+++ 09 DivideConquer/BOJ2630/BOJ1074/src/Main.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 
 public class Main {
-    static int[][] field;
-    static int count = 0;
+
+    static int number = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -13,12 +13,10 @@ public class Main {
         int r = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
         int length = (int) Math.pow(2,n);
-        field = new int[length][length];
 
         divConquer(length,r, c,0,0);
+        System.out.println(number);
 
-
-        System.out.print(field[r][c]);
 
     }
 
@@ -30,15 +28,15 @@ public class Main {
                divConquer(length / 2, rows, column,  startRows, startColumn);
            }
            if(rows <= startRows + length/2 -1 && column > startColumn +length/2 -1){
-               field[startRows][startColumn+length/2] = field[startRows][startColumn] + (int)Math.pow(length/2, 2);
+               number = number + (int)Math.pow(length/2 , 2);
                divConquer(length/2, rows, column,  startRows, startColumn + length/2 );
            }
-           if(rows > length/2 -1 && column <= length/2 -1) {
-               field[startRows+length/2][startColumn] = field[startRows][startColumn] + 2*(int)Math.pow(length/2, 2);
+           if(rows > startRows + length/2 -1 && column <= startColumn +length/2 -1) {
+               number = number + 2 *(int)Math.pow(length/2 , 2);
                divConquer(length / 2, rows, column,  startRows + length/2 ,startColumn);
            }
-           if(rows > length/2 -1 && column > length/2 -1) {
-               field[startRows+length/2][startColumn+length/2] = field[startRows][startColumn] + 3*(int)Math.pow(length/2, 2);
+           if(rows > startRows + length/2 -1 && column > startColumn + length/2 -1) {
+               number = number + 3 *(int)Math.pow(length/2 , 2);
                divConquer(length / 2, rows, column,startRows + length/2 ,startColumn + length/2);
            }
        }
